f01e5c36319a16dc11909d2ca24c937b3eef2a11
BenPhan <benphan1999@gmail.com>
2019-11-02 22:41:20 +0700

    CM02 - Lambda Ex01

--- src/ex01/Ex01.java
+++ src/ex01/Ex01.java
@@ -1,5 +1,7 @@
 package ex01;
 
+import java.util.function.BiFunction;
+
 public class Ex01 {
 	public static void main(String[] args) {
 		int a = 5, b = 10;
@@ -7,6 +9,7 @@ public class Ex01 {
 		System.out.println("Tong:" + c);
 		int d = mul(a, b);
 		System.out.println("Tich:" + d);
+		lambda();
 	}
 
 	private static int sum(int a, int b) {
@@ -15,5 +18,11 @@ public class Ex01 {
 
 	private static int mul(int a, int b) {
 		return a * b;
+	}
+	private static void lambda() {
+		BiFunction<Double, Double, Double> sum = (a,b)->a+b;
+		BiFunction<Double, Double, Double> sub = (a,b)->a-b;
+		BiFunction<Double, Double, Double> mul = (a,b)->a*b;
+		BiFunction<Double, Double, Double> div = (a,b)->a/b;
 	}
 }
